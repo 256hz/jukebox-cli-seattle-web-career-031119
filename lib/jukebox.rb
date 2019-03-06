3eccdfe31454b1dfe96a6a5c328d820b91a9e897
Abe D <iaryiou111@gmail.com>
2019-03-06 23:42:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,11 +29,7 @@ def get_input
 end
 
 def help
-  puts "I accept the following commands:\n
-- help : displays this help message\n
-- list : displays a list of songs you can play\n
-- play : lets you choose a song to play\n
-- exit : exits this program"
+  puts "I accept the following commands:\n- help : displays this help message\n- list : displays a list of songs you can play\n- play : lets you choose a song to play\n- exit : exits this program"
   get_input
 end
 
