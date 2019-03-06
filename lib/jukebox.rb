b8e9e145f20a431426e20f9b083b87a578f2a448
Abe D <iaryiou111@gmail.com>
2019-03-06 23:36:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,8 +20,8 @@ def get_input
       list
     when "play"
       play
-    when exit
-      quit
+    when "exit"
+      exit_jukebox
     else
       puts "I don't recognize that command."
       get_input
@@ -36,3 +36,36 @@ def help
 - exit : exits this program"
   get_input
 end
+
+def list
+  songs.each.with_index(1) do |song|
+    puts "#{index}. #{song}"
+  end
+  get_input
+end
+
+def play
+  puts "Please enter a song name or number:"
+  song = gets.chomp
+  tracklist = []
+  songs.each do |no|
+    tracklist << no + 1
+  end
+  case song
+    when tracklist.include?(song)
+      puts "Playing #{songs[song]}"
+    when songs.include?(song)
+      puts "Playing #{song}"
+    else
+      puts "Invalid input, please try again"
+      play
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye."
+  exit
+end
+
+def run
+  
