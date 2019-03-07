2536b6d5e741d514ef7413ac1dc4fee43fc61bad
Abe D <iaryiou111@gmail.com>
2019-03-07 00:41:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,7 +34,6 @@ end
 
 def help(songs)
   puts "I accept the following commands:\n- help : displays this help message\n- list : displays a list of songs you can play\n- play : lets you choose a song to play\n- exit : exits this program"
-  run(songs)
 end
 
 def list(songs)
