998b3684992b2cdf2c5f6f7d6020a6eeea244efe
Abe D <iaryiou111@gmail.com>
2019-03-07 00:39:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,39 +12,39 @@ $songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def run #(songs)
+def run(songs)
   #help(songs) if first_time = true
   puts "Please enter a command:"
   command = gets.chomp
   case command
     when "help"
-      help #(songs)
+      help(songs)
     when "list"
-      list #(songs)
+      list(songs)
     when "play"
-      play #(songs)
+      play(songs)
     when "exit"
       exit_jukebox
     else
       puts "I don't recognize that command."
-      run #(songs)
+      run(songs)
   end
 end
 
 
-def help #(songs)
+def help(songs)
   puts "I accept the following commands:\n- help : displays this help message\n- list : displays a list of songs you can play\n- play : lets you choose a song to play\n- exit : exits this program"
-  run #(songs)
+  run(songs)
 end
 
-def list #(songs)
+def list(songs)
   songs.each.with_index(1) do |song, index|
     puts "#{index}. #{song}"
   end
-  run #(songs)
+  run(songs)
 end
 
-def play #(songs)
+def play(songs)
   puts "Please enter a song name or number:"
   song = gets.chomp
   tracklist = []
@@ -59,7 +59,7 @@ def play #(songs)
     else
       puts "Invalid input, please try again"
   end
-  run #(songs)
+  run(songs)
 end
 
 def exit_jukebox
